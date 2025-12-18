e01dbf96ca92d6a24bf58bdc12ed24216830b957
Vikaash-ai <vikaashdummy@gmail.com>
2025-12-18 20:59:37 +1300

    Refactor: reorganize and restore lesson files with updated content

---
+++ Lesson 1/main.py
@@ -0,0 +1,14 @@
+#Greetings
+print("hello")
+
+#print my name
+print("I am Vikaash")
+
+# Print about my fav fact
+print("I love coding")
+
+# Simple Calculation
+
+print("the result of 100 + 90 is:", 100 + 90)
+
+print("yay")
